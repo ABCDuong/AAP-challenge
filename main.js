9fcd156cc7f863dd0364fd4251f326c1abd3e497
ABCDuong <stevenduong22@gmail.com>
2020-12-12 00:33:37 -0800

    add constants, loader for async data, pet list

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 function petListings() {
   return {
+    isLoading: true,
+    petList: [],
     selectedPetListState: 'Default',
     setPetListFilter(state) {
       this.selectedPetListState = state;
+
+
+    },
+    async getPetList() {
+      const url = 'https://api.adoptapet.com/search/pet_search?key=e41b6bf1618d053c31d524d479c14b5t&geo_range=50&city_or_zip=91770&species=dog&output=json';
+      this.isLoading = true;
+      let response = await fetch(url);
+      let petData = await response.json();
+      await this.getPetDetails(petData.pets);
+      this.petList = petData.pets;
+      console.log({petData: JSON.parse(JSON.stringify(this.petList))});
+      this.isLoading = false;
+    },
+    async getPetDetails(petList) {
+      for (const pet of petList) {
+        let response = await fetch(pet.details_url);
+        let petDetailData = await response.json();
+        pet.details = petDetailData.pet;
+        pet.details.special_needs = 1;
+      }
     }
   }
 }
+
--- shared/constants.js
+++ shared/constants.js
@@ -4,3 +4,22 @@ const petListingState = [
   'Special Needs',
   'Adopted'
 ];
+
+const petInfoAlert = {
+  act_quickly: 'Act Quickly',
+  adopted: 'Adopted',
+  bonded_pair: 'Bonded pair',
+  special_needs: 'Special needs'
+};
+
+const petInfoDetails = {
+  housetrained: 'House-trained',
+  good_with_cats: 'Good with cats',
+  good_with_dogs: 'Good with dogs',
+  good_with_kids: 'Good with kids',
+  
+  not_housetrained: 'Not house-trained',
+  not_good_with_cats: 'Not good with cats',
+  not_good_with_dogs: 'Not good with dogs',
+  not_good_with_kids: 'Not good with kids'
+}
